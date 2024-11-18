0a4907b64549976424967e5f6d0327c949d5901b
Niranjan Gopalan <niranjangopalan948@gmail.com>
2024-11-18 19:26:07 +0000

    added readme file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="davis_summary_vac",
-    version="0.1.0",
+    version="0.1.1",
     author="Niranjan Gopalan",
     author_email="niranjangopalan948@gmail.com",
     description="A package for analyzing CSV files and generating summary reports in HTML",
